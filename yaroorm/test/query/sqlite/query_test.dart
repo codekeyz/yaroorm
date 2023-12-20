da9f693f46c6460e75de218f4a4dc44e33bcd030
Amos Godwin <amosgodwin500@gmail.com>
2023-12-20 18:34:32 +0100

    added query tests for  Postgres SQL

--- yaroorm/test/query/sqlite/query_test.dart
+++ yaroorm/test/query/sqlite/query_test.dart
@@ -5,12 +5,15 @@ import '../../helpers/drivers.dart';
 
 void main() {
   late DatabaseDriver driver;
+  late DatabaseDriver postgresSqlDriver;
+
 
   setUpAll(() {
     driver = DatabaseDriver.init(sqliteConnection);
+    postgresSqlDriver = DatabaseDriver.init(postgresSqlConnection);
   });
 
-  group('Query.query', () {
+  group('SQLite Query.query', () {
     group('when `.orderByAsc`', () {
       test(' of level 1', () {
         expect(
@@ -52,6 +55,58 @@ void main() {
       test(' of level 3', () {
         expect(
           Query.query('users', driver)
+              .where('firstname', 'not null')
+              .orderByDesc('lastname')
+              .orderByDesc('age')
+              .statement,
+          'SELECT * FROM users WHERE firstname IS NOT NULL ORDER BY lastname DESC, age DESC;',
+        );
+      });
+    });
+  });
+
+  group('Postgre SQL Query.query', () {
+    group('when `.orderByAsc`', () {
+      test(' of level 1', () {
+        expect(
+          Query.query('users', postgresSqlDriver).orderByAsc('firstname').statement,
+          'SELECT * FROM users ORDER BY firstname ASC;',
+        );
+      });
+
+      test(' of level 2', () {
+        expect(
+          Query.query('users', postgresSqlDriver).orderByAsc('firstname').orderByAsc('lastname').statement,
+          'SELECT * FROM users ORDER BY firstname ASC, lastname ASC;',
+        );
+      });
+
+      test(' of level 3', () {
+        expect(
+          Query.query('users', postgresSqlDriver).orderByAsc('firstname').orderByDesc('lastname').orderByAsc('age').statement,
+          'SELECT * FROM users ORDER BY firstname ASC, lastname DESC, age ASC;',
+        );
+      });
+    });
+
+    group('when `.orderByDesc`', () {
+      test(' of level 1', () {
+        expect(
+          Query.query('users', postgresSqlDriver).orderByDesc('firstname').statement,
+          'SELECT * FROM users ORDER BY firstname DESC;',
+        );
+      });
+
+      test(' of level 2', () {
+        expect(
+          Query.query('users', postgresSqlDriver).orderByDesc('firstname').orderByDesc('lastname').statement,
+          'SELECT * FROM users ORDER BY firstname DESC, lastname DESC;',
+        );
+      });
+
+      test(' of level 3', () {
+        expect(
+          Query.query('users', postgresSqlDriver)
               .where('firstname', 'not null')
               .orderByDesc('lastname')
               .orderByDesc('age')
