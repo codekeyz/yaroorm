6c294a6165d469110dc7db071089eb422be7e245
Chima Precious <ositachima.co@gmail.com>
2023-12-21 14:21:28 +0000

    add cookies and signed cookies on application controller

--- yaroo/lib/http/http.dart
+++ yaroo/lib/http/http.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: camel_case_types
-
 import 'dart:async';
+import 'dart:io';
 
 import 'package:yaroo/src/_reflector/reflector.dart';
 import 'package:yaroo/src/core.dart';
@@ -28,22 +27,28 @@ abstract class ApplicationController extends AppInstance {
 
   Map<String, dynamic> get params => request.params;
 
-  Map<String, dynamic> get query => request.query;
+  Map<String, dynamic> get queryParams => request.query;
 
   Map<String, dynamic> get headers => request.headers;
+
+  Session? get session => request.session;
+
+  List<Cookie> get cookies => request.cookies;
+
+  List<Cookie> get signedCookies => request.signedCookies;
 
   get body => request.body;
 
   Response badRequest([String? message]) {
     const status = 422;
     if (message == null) return response.status(status);
-    return response.json({'message': message}, statusCode: status);
+    return response.json({'error': message}, statusCode: status);
   }
 
   Response notFound([String? message]) {
     const status = 404;
     if (message == null) return response.status(status);
-    return response.json({'message': message}, statusCode: status);
+    return response.json({'error': message}, statusCode: status);
   }
 
   Response jsonResponse(data, {int statusCode = 200}) {
