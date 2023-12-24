aaa5bfbcbc49e84b98c5e295eb8065ee20a37ec2
Chima Precious <ositachima.co@gmail.com>
2023-12-24 13:56:40 +0000

    tiny fix

--- yaroorm/test/integration/mysql.e2e.dart
+++ yaroorm/test/integration/mysql.e2e.dart
@@ -7,7 +7,12 @@ import 'base.dart';
 final _driver = DatabaseDriver.init(mysqlConnection);
 
 void main() {
-  setUpAll(() => Future.sync(() => _driver.connect(secure: true)));
+  setUpAll(() async {
+    final driver = await _driver.connect(secure: true);
+    print('We completed');
+
+    assert(driver.isOpen, 'Driver is not open');
+  });
 
   group('MySQL', () => runIntegrationTest(_driver));
 }
