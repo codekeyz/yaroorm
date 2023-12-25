d997e10fd796f8a452ef48273fece179ddb7ddc7
Chima Precious <ositachima.co@gmail.com>
2023-12-25 13:07:03 +0000

    tiny fix

--- yaroorm/e2e_test.sh
+++ yaroorm/e2e_test.sh
@@ -10,6 +10,6 @@ pattern="*.e2e.dart"
 for file in "$TEST_DIRECTORY"/$pattern; do
     filename=$(basename "$file")
     test_name="${filename%%.*}"
-    echo "Running tests for: $test_name"
+    echo "Running tests for: $test_name 📦"
     dart test "$file" --coverage=coverage --fail-fast
 done
