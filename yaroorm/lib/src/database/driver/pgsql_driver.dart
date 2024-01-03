a651fee0d1aa75c8b09bee2f91c1f8abc66a9a92
Amos Godwin <amosgodwin500@gmail.com>
2024-01-04 00:32:44 +0100

    removed print

--- yaroorm/lib/src/database/driver/pgsql_driver.dart
+++ yaroorm/lib/src/database/driver/pgsql_driver.dart
@@ -96,7 +96,6 @@ class PostgreSqlDriver implements DatabaseDriver {
   Future<bool> hasTable(String tableName) async {
     final result = await _execRawQuery(
         '''SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE' AND table_name='$tableName';''');
-    print(result);
     if (result.isEmpty) return false;
     return true;
   }
