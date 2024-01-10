646450da6aa781e8339d6ec28adaf663961b03c3
Chima Precious <ositachima.co@gmail.com>
2024-01-10 16:54:25 +0000

    run relations e2e test for mysql

--- yaroorm/test/integration/mysql.e2e.dart
+++ yaroorm/test/integration/mysql.e2e.dart
@@ -3,6 +3,7 @@ import 'package:yaroorm/yaroorm.dart';
 
 import '../fixtures/orm_config.dart' as db;
 import 'e2e_basic.dart';
+import 'e2e_relation.dart';
 import 'mysql.e2e.reflectable.dart';
 
 void main() async {
@@ -10,5 +11,9 @@ void main() async {
 
   DB.init(db.config);
 
-  group('MySQL', () => runBasicE2ETest('moo_mysql'));
+  group('MySQL', () {
+    group('Basic E2E Test', () => runBasicE2ETest('moo_mysql'));
+
+    group('Relation E2E Test', () => runRelationsE2ETest('moo_mysql'));
+  });
 }
