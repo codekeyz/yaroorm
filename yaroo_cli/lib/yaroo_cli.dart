de034e3b55ff75af10f7cc173f3ea41eb2b212a6
Chima Precious <ositachima.co@gmail.com>
2024-01-11 18:32:27 +0000

    add boilerplate for yaroo cli

---
+++ yaroo_cli/lib/yaroo_cli.dart
@@ -0,0 +1,10 @@
+/// yaroo_cli, Yaroo command-line tool
+///
+/// ```sh
+/// # activate yaroo_cli
+/// dart pub global activate yaroo_cli
+///
+/// # see usage
+/// yaroo --help
+/// ```
+library yaroo_cli;
