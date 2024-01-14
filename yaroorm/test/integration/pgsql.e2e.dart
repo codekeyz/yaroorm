aad0f688d7d405369abe99e9e185e44d3e96aec6
Amos Godwin <amosgodwin500@gmail.com>
2024-01-15 00:46:27 +0100

    updated integration test

--- yaroorm/test/integration/pgsql.e2e.dart
+++ yaroorm/test/integration/pgsql.e2e.dart
@@ -1,7 +1,8 @@
 import 'package:test/test.dart';
 import 'package:yaroorm/yaroorm.dart';
-import '../fixtures/orm_config.dart' as db;
-import 'base.dart';
+import 'fixtures/orm_config.dart' as db;
+import 'e2e_basic.dart';
+import 'e2e_relation.dart';
 import 'pgsql.e2e.reflectable.dart';
 
 void main() async {
@@ -9,5 +10,10 @@ void main() async {
 
   DB.init(db.config);
 
-  group('Postgres', () => runIntegrationTest('foo_pgsql'));
+  group('Postgres', () {
+    group('Basic E2E Test', () => runBasicE2ETest('foo_pgsql'));
+
+    group('Relation E2E Test', () => runRelationsE2ETest('foo_pgsql'));
+  });
+
 }
