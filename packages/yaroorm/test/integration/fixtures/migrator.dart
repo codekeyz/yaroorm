f1f36b247df8e5256df5fe68a824e4abaa8f781b
Chima Precious <ositachima.co@gmail.com>
2024-03-03 18:12:49 +0000

    tiny fix

--- packages/yaroorm/test/integration/fixtures/migrator.dart
+++ packages/yaroorm/test/integration/fixtures/migrator.dart
@@ -20,7 +20,6 @@ Future<void> runMigrator(String connectionName, String command) async {
   print('> dart ${commands.join(' ')}\n');
 
   final result = await Process.run('dart', commands);
-  stdout.write(result.stdout);
   stderr.write("""--------------------------------------------
       \n${result.stderr}\n
       --------------------------------------------""");
