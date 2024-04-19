6a68243cb46bf8fca26ab9e944dac90f624be31f
Chima Precious <ositachima.co@gmail.com>
2024-04-19 23:58:12 +0000

    add yaroorm executable

--- bin/yaroorm.dart
+++ bin/yaroorm.dart
@@ -1,3 +1,35 @@
-void main() {
-  print('Hello world');
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:yaroorm/src/cli/logger.dart';
+
+void main(List<String> args) async {
+  final migratorDirectoryPath = migratorPath;
+  final dartFile = path.join(migratorDirectoryPath, 'migrator.dart');
+  final aotFilePath = path.join(migratorDirectoryPath, 'migrator');
+
+  if (!File(dartFile).existsSync()) {
+    logger.err('🗙 Migrator file does not exist');
+    exit(0);
+  }
+
+  final aotFile = File(aotFilePath);
+  if (!aotFile.existsSync()) {
+    /// TODO(codekeyz): add checksum check for invalidating aot
+    Process.start('dart', ['compile', 'exe', dartFile, '-o', aotFilePath], mode: ProcessStartMode.detached);
+  }
+
+  late Process process;
+
+  if (aotFile.existsSync()) {
+    process = await Process.start(aotFilePath, args);
+  } else {
+    process = await Process.start('dart', ['run', dartFile, ...args]);
+  }
+
+  stdout.addStream(process.stdout);
+  stderr.addStream(process.stderr);
+}
+
+String get migratorPath {
+  return path.join(Directory.current.path, '.dart_tool', 'yaroorm/bin');
 }
