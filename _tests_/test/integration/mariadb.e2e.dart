670de66ffe21fcaab66cb0f79b7b7dc7c9cc1511
Chima Precious <ositachima.co@gmail.com>
2024-05-01 12:57:01 +0000

    restore relations tes

--- _tests_/test/integration/mariadb.e2e.dart
+++ _tests_/test/integration/mariadb.e2e.dart
@@ -1,14 +1,15 @@
 import 'package:test/test.dart';
 
-import '../../database/database.dart';
+import '../../database/database.dart' as db;
 import 'e2e_basic.dart';
+import 'e2e_relation.dart';
 
 void main() async {
-  initializeORM();
+  db.initializeORM();
 
   group('MariaDB', () {
     group('Basic E2E Test', () => runBasicE2ETest('bar_mariadb'));
 
-    // group('Relation E2E Test', () => runRelationsE2ETest('bar_mariadb'));
+    group('Relation E2E Test', () => runRelationsE2ETest('bar_mariadb'));
   });
 }
--- _tests_/test/integration/mysql.e2e.dart
+++ _tests_/test/integration/mysql.e2e.dart
@@ -1,14 +1,15 @@
 import 'package:test/test.dart';
 
-import '../../database/database.dart';
+import '../../database/database.dart' as db;
 import 'e2e_basic.dart';
+import 'e2e_relation.dart';
 
 void main() async {
-  initializeORM();
+  db.initializeORM();
 
   group('MySQL', () {
     group('Basic E2E Test', () => runBasicE2ETest('moo_mysql'));
 
-    // group('Relation E2E Test', () => runRelationsE2ETest('moo_mysql'));
+    group('Relation E2E Test', () => runRelationsE2ETest('moo_mysql'));
   });
 }
