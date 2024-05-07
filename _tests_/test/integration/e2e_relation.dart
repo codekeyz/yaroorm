bb4432adb8ddc5f6b823ec775d0afe56a7a3afe2
Chima Precious <ositachima.co@gmail.com>
2024-05-07 01:06:01 +0000

    fix formatting

--- _tests_/test/integration/e2e_relation.dart
+++ _tests_/test/integration/e2e_relation.dart
@@ -51,24 +51,15 @@ void runRelationsE2ETest(String connectionName) {
         orderBy: [OrderPostBy.title(order: OrderDirection.desc)],
       );
       expect(posts, hasLength(3));
-      expect(
-          posts.map((e) => {
-                'id': e.id,
-                'title': e.title,
-                'desc': e.description,
-                'userId': e.userId
-              }),
-          [
-            {'id': 3, 'title': 'Coo Kie 3', 'desc': 'foo bar 6', 'userId': 1},
-            {'id': 2, 'title': 'Bee Moo 2', 'desc': 'foo bar 5', 'userId': 1},
-            {'id': 1, 'title': 'Aoo bar 1', 'desc': 'foo bar 4', 'userId': 1}
-          ]);
+      expect(posts.map((e) => {'id': e.id, 'title': e.title, 'desc': e.description, 'userId': e.userId}), [
+        {'id': 3, 'title': 'Coo Kie 3', 'desc': 'foo bar 6', 'userId': 1},
+        {'id': 2, 'title': 'Bee Moo 2', 'desc': 'foo bar 5', 'userId': 1},
+        {'id': 1, 'title': 'Aoo bar 1', 'desc': 'foo bar 4', 'userId': 1}
+      ]);
     });
 
     test('should fetch posts with owner', () async {
-      final posts = await PostQuery.driver(driver)
-          .withRelations((post) => [post.owner])
-          .findMany();
+      final posts = await PostQuery.driver(driver).withRelations((post) => [post.owner]).findMany();
 
       final owner = await posts.first.owner.value;
       expect(
